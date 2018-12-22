56c0deeb3a5b0981d7271acc8228227a98bd75ce
Matheus Picioli <matheus.picioli98@gmail.com>
2018-12-22 19:13:43 -0200

    Projeto

---
+++ ByteBank/ByteBank.SistemaAgencia/Estagiario.cs
@@ -0,0 +1,29 @@
+﻿using ByteBank.Modelos.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    public class Estagiario : Funcionario
+    {
+        public Estagiario(double salario, string cpf)
+            : base(salario, cpf)
+        {
+        }
+        public override void AumentarSalario()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override double GetBonificacao()
+        {
+            // COMO É UMA IMPLEMENTAÇÃO FORA
+            // AO PROJETO RÁIZ, É ESCRITO APENAS
+            // COMO PROTECTED E NÃO INTERNAL PROTECTED
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,32 @@
+﻿using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(0001,7687150);
+
+            // FuncionarioAutenticavel implementa a AutenticacaoHelper
+            // no mesmo projeto, portanto não temos erros aqui
+            FuncionarioAutenticavel carlos = null;
+            carlos.Autenticar("testesenha");
+
+            // AutenticacaoHelper é uma classe interna
+            // portanto não pode ser instanciada fora
+            // de seu projeto inicial
+            //AutenticacaoHelper autenticacao = new AutenticacaoHelper();
+            //autenticacao.CompararSenhas("senha true","senha false");
+
+            Console.WriteLine(conta.Numero);
+            Console.ReadLine();
+        }
+    }
+}
